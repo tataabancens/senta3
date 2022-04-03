d209a537209489930fee09d1e33d9860e81dcfb6
Cami <cponce@itba.edu.ar>
2022-04-03 14:57:15 -0300

    feat: create reservation dao
    Co-authored-by: Alberto <aabancens@itba.edu.ar>

---
+++ paw-webapp/persistence/src/main/java/ar/edu/itba/paw/persistence/ReservationJdbcDao.java
@@ -0,0 +1,58 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.model.Reservation;
+import ar.edu.itba.paw.model.Restaurant;
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.persistance.ReservationDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class ReservationJdbcDao implements ReservationDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    private static final RowMapper<Reservation> ROW_MAPPER = ((resultSet, i) ->
+            new Reservation(resultSet.getLong("reservationId"),
+                    resultSet.getLong("restaurantId"),
+                    resultSet.getTimestamp("ReservationDate")));
+
+    @Autowired
+    public ReservationJdbcDao(final DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(ds)
+            .withTableName("reservation")
+            .usingGeneratedKeyColumns("reservationId");
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS reservation ("
+                + "reservationId SERIAL PRIMARY KEY,"
+                + "restaurantId varchar(100) NOT NULL,"
+                + "reservationDate timestamp,"
+                + ")");
+    }
+
+    @Override
+    public Optional<Reservation> getReservationById(long id) {
+        List<Reservation> query = jdbcTemplate.query("SELECT * FROM restaurant WHERE restaurantId", new Object[]{id}, ROW_MAPPER);
+        return query.stream().findFirst();
+    }
+    @Override
+    public Reservation create(long restaurantId, Timestamp reservationDate) {
+        final Map<String, Object> reservationData = new HashMap<>();
+        reservationData.put("restaurantId", restaurantId);
+        reservationData.put("reservationDate", reservationDate);
+
+        int reservationId = jdbcInsert.execute(reservationData);
+        return new Reservation(reservationId, restaurantId, reservationDate);
+    }
+}
