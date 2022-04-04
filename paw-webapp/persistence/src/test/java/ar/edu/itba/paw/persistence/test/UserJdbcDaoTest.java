ccb637ec5e8018f2acd232e5ccd38f3ab4a23d77
Alberto <tataabancens@gmail.com>
2022-04-04 19:14:28 -0300

    Feat: Test config added

--- paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/test/UserJdbcDaoTest.java
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/test/UserJdbcDaoTest.java
@@ -6,61 +6,63 @@ import org.hsqldb.jdbc.JDBCDriver;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
 public class UserJdbcDaoTest {
+
+    private static final String USER_TABLE = "users";
+    private static final String USERNAME = "pepe";
 
     private UserJdbcDao userDao ;
     private JdbcTemplate jdbcTemplate;
-
     private SimpleJdbcInsert jdbcInsert;
+
+    @Autowired
+    private DataSource ds;
 
     @Before
     public void setUp(){
-        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
-        ds.setDriverClass(JDBCDriver.class);
-        ds.setUrl("jdbc:hsqldb:mem:paw");
-        ds.setUsername("user");
-        ds.setPassword("");
         userDao = new UserJdbcDao(ds);
         jdbcTemplate = new JdbcTemplate(ds);
         jdbcInsert = new SimpleJdbcInsert(ds)
-                .withTableName("users")
-                .usingGeneratedKeyColumns("id");
-
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS users (" +
-                "userId INTEGER IDENTITY PRIMARY KEY," +
-                "username varchar(100)," +
-                "password varchar(100)" +
-                ")");
+                .withTableName(USER_TABLE)
+                .usingGeneratedKeyColumns("userId");
     }
 
     @Test
     public void testCreateUser(){
         // 1. Precondiciones
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, USER_TABLE);
 
         // 2. Ejercitacion
-        User user = userDao.create("pepe", "pass");
+        User user = userDao.create(USERNAME, "pass");
 
         // 3. PostCondiciones
         Assert.assertNotNull(user);
-        Assert.assertEquals("pepe", user.getUsername());
-        Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "users"));
+        Assert.assertEquals(USERNAME, user.getUsername());
+        Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, USER_TABLE));
     }
 
     @Test
     public void testFindUserByIdDoesntExist(){
         // 1. Precondiciones
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, USER_TABLE);
 
         // 2. Ejercitacion
         Optional<User> maybeUser = userDao.getUserById(1);
@@ -74,9 +76,9 @@ public class UserJdbcDaoTest {
     public void testFindUserByIdExists(){
         // 1. Precondiciones
         Map<String, String> map = new HashMap<>();
-        map.put("username", "pepe");
+        map.put("username", USERNAME);
 
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, USER_TABLE);
         Number key = jdbcInsert.executeAndReturnKey(map);
 
         // 2. Ejercitacion
@@ -84,6 +86,6 @@ public class UserJdbcDaoTest {
 
         // 3. PostCondiciones
         Assert.assertTrue(maybeUser.isPresent());
-        Assert.assertEquals("pepe", maybeUser.get().getUsername());
+        Assert.assertEquals(USERNAME, maybeUser.get().getUsername());
     }
 }
