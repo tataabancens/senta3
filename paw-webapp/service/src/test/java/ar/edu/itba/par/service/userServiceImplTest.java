07c90d9f8f4ccdadfff1c357e40f4ffba4afde2b
Cami <cponce@itba.edu.ar>
2022-04-05 16:00:09 -0300

    feat: add mockito support

---
+++ paw-webapp/service/src/test/java/ar/edu/itba/par/service/userServiceImplTest.java
@@ -0,0 +1,43 @@
+package ar.edu.itba.par.service;
+
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.persistance.UserDao;
+import ar.edu.itba.paw.service.UserServiceImpl;
+import org.junit.Assert;
+import org.junit.Before;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class userServiceImplTest {
+
+    @InjectMocks
+    private UserServiceImpl userService = new UserServiceImpl();
+
+    @Mock
+    private UserDao userDao;
+
+
+    @Test
+    public void testCreateUser() {
+        User user = new User(1, "pepe", "pepe");
+        Mockito.when(userDao.create(Mockito.anyString(), Mockito.anyString())).thenReturn(user);
+
+        try {
+            User u = userService.create("pepe", "pepe");
+        }
+        catch (Exception e){
+            Assert.fail("unexpected error during create user");
+        }
+
+        // no usar verify !! testeamos comportamiento no implementacion!
+
+
+    }
+}
