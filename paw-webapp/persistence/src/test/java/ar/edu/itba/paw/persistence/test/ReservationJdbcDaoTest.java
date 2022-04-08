1f4e550790b712525644bd2796b56b2b2548cf1d
Alberto <tataabancens@gmail.com>
2022-04-08 20:22:44 -0300

    feat: more reservation test added

--- paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/test/ReservationJdbcDaoTest.java
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/test/ReservationJdbcDaoTest.java
@@ -18,19 +18,27 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class ReservationJdbcDaoTest {
 
     private static final String RESERVATION_TABLE = "reservation";
-    private static final String ORDER_ITEM_TABLE = "reservation";
+    private static final String ORDER_ITEM_TABLE = "orderItem";
+//    private static final String CUSTOMER_TABLE = "customer";
+//    private static final String RESTAURANT_TABLE = "restaurant";
+//    private static final String DISH_TABLE = "dish";
 
     private ReservationDao reservationDao ;
     private JdbcTemplate jdbcTemplate;
     private SimpleJdbcInsert jdbcInsertReservation;
     private SimpleJdbcInsert jdbcInsertOrderItem;
+//    private SimpleJdbcInsert jdbcInsertCustomer;
+//    private SimpleJdbcInsert jdbcInsertRestaurant;
+//    private SimpleJdbcInsert jdbcInsertDish;
 
     @Autowired
     private DataSource ds;
@@ -41,7 +49,16 @@ public class ReservationJdbcDaoTest {
         jdbcTemplate = new JdbcTemplate(ds);
         jdbcInsertReservation = new SimpleJdbcInsert(ds)
                 .withTableName(RESERVATION_TABLE)
-                .usingGeneratedKeyColumns("userId");
+                .usingGeneratedKeyColumns("reservationId");
+//        jdbcInsertCustomer = new SimpleJdbcInsert(ds)
+//                .withTableName(CUSTOMER_TABLE)
+//                .usingGeneratedKeyColumns("customerId");
+//        jdbcInsertRestaurant = new SimpleJdbcInsert(ds)
+//                .withTableName(RESTAURANT_TABLE)
+//                .usingGeneratedKeyColumns("restaurantId");
+//        jdbcInsertDish = new SimpleJdbcInsert(ds)
+//                .withTableName(DISH_TABLE)
+//                .usingGeneratedKeyColumns("dishId");
         jdbcInsertOrderItem = new SimpleJdbcInsert(ds)
                 .withTableName(ORDER_ITEM_TABLE);
     }
@@ -61,5 +78,17 @@ public class ReservationJdbcDaoTest {
         for (int i = 0; i < maybeList.size(); i++) {
             Assert.assertTrue(testList.get(i).equals(maybeList.get(i)));
         }
+    }
+
+    @Test
+    public void testGetOrderItemsByReservationIdDoesntExists() {
+        // 1. Precondiciones
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, ORDER_ITEM_TABLE);
+
+        // 2. Ejercitacion
+        List<OrderItem> maybeList = reservationDao.getOrderItemsByReservationId(1);
+
+        // 3. PostCondiciones
+        Assert.assertTrue(maybeList.isEmpty());
     }
 }
