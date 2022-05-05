0c63df416a8c1ed8051137cf4998562322f792f9
Uriel <umihura@itba.edu.ar>
2022-05-05 14:41:18 -0300

    Feat: customer y image not found
    no los supe testear

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/utilities/ExceptionController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/utilities/ExceptionController.java
@@ -1,8 +1,6 @@
 package ar.edu.itba.paw.webapp.controller.utilities;
 
-import ar.edu.itba.paw.webapp.exceptions.DishNotFoundException;
-import ar.edu.itba.paw.webapp.exceptions.ReservationNotFoundException;
-import ar.edu.itba.paw.webapp.exceptions.RestaurantNotFoundException;
+import ar.edu.itba.paw.webapp.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -30,6 +28,18 @@ public class ExceptionController {
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public ModelAndView handleDishNotFoundException(){
         return new ModelAndView("error/dishNotFound");
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ModelAndView handleCustomerNotFoundException(){
+        return new ModelAndView("error/customerNotFound");
+    }
+
+    @ExceptionHandler(ImageNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ModelAndView handleImageNotFoundException(){
+        return new ModelAndView("error/imageNotFound");
     }
 
 
