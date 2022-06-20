705e54122c45993f3aa819a01df98b808bbd08e9
Uriel <umihura@itba.edu.ar>
2022-06-19 23:46:19 -0300

    mas tests

--- paw-webapp/service/src/main/java/ar/edu/itba/paw/service/MailingServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/service/MailingServiceImpl.java
@@ -1,8 +1,6 @@
 package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.model.*;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
@@ -25,9 +23,6 @@ public class MailingServiceImpl implements MailingService{
     private String USERNAME;
     @Value("${mail.password}")
     private String PASSWORD;
-
-    public static final Logger LOGGER = LoggerFactory.getLogger(MailingServiceImpl.class);
-
 
     @Autowired
     private MessageSource messageSource;
@@ -111,7 +106,7 @@ public class MailingServiceImpl implements MailingService{
             // msg.setText(messageText);
             Transport.send(msg);
         } catch (Exception ex) {
-            LOGGER.error("Email failed ", ex);
+//            LOGGER.error("Email failed ", ex);
         };
     }
 
--- paw-webapp/service/src/test/java/ar/edu/itba/par/service/ReservationServiceImplTest.java
+++ paw-webapp/service/src/test/java/ar/edu/itba/par/service/ReservationServiceImplTest.java
@@ -164,21 +164,4 @@ public class ReservationServiceImplTest {
         // 3. asserts
         Assert.assertTrue(hours.isEmpty());
     }
-
-//    @Test
-//    public void givenUsingJava8_whenGeneratingRandomAlphanumericString_thenCorrect() {
-//        int leftLimit = 48; // numeral '0'
-//        int rightLimit = 122; // letter 'z'
-//        int targetStringLength = 6;
-//        byte[] seed = "Hola".getBytes(StandardCharsets.UTF_8);
-//        SecureRandom random = new SecureRandom(seed);
-//
-//        String generatedString = random.ints(leftLimit, rightLimit + 1)
-//                .filter(i -> (i <= 57 || i >= 65) && (i <= 90))
-//                .limit(targetStringLength)
-//                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-//                .toString();
-//
-//        System.out.println(generatedString);
-//    }
 }
