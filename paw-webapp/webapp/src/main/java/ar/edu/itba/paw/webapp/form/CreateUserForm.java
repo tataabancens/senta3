c502d680982a873c8d8772e4873af843e0c08c12
Uriel <umihura@itba.edu.ar>
2023-01-17 17:50:59 -0300

    Fix: create unique username contstraint

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/form/CreateUserForm.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/form/CreateUserForm.java
@@ -1,7 +1,18 @@
 package ar.edu.itba.paw.webapp.form;
 
+import ar.edu.itba.paw.webapp.form.customValidator.UsernameConstraint;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class CreateUserForm {
+
+    @UsernameConstraint
+    @Size(min = 1, max = 50)
+    @Pattern(regexp = "^[a-zA-Z 0-9,.'-]+$")
     private String username;
+
+
     private String role;
 
     public String getUsername() {
