4b6becf90c9cc587964ce4bd5ad13cd4252b7bc4
GonzaloRossin <gonzarossin@gmail.com>
2023-07-18 01:05:14 -0300

    i18n 404 page

--- paw-webapp/spa-client/src/pages/NoPage.tsx
+++ paw-webapp/spa-client/src/pages/NoPage.tsx
@@ -1,13 +1,15 @@
 import { Grid, Paper, Typography } from "@mui/material";
 import { FC } from "react";
 import { themePalette } from "../config/theme.config";
+import { useTranslation } from "react-i18next";
 
 
 const NoPage: FC = () => {
+    const { t } = useTranslation();
     return(
             <Grid container component={Paper} sx={{width: 500, height: 250, position: "absolute", top: "40%", right:"40%", backgroundColor: themePalette.PURPLE}} alignItems="center">
                 <Grid item xs={12}>
-                    <Typography variant="h5" color="white" align="center">404: The route requested does not exist</Typography>
+                    <Typography variant="h5" color="white" align="center">{t('pageDoesNotExist')}</Typography>
                 </Grid>
             </Grid>
     );
